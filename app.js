9f97244eeb8152e983e8a2b6468049b233c261c2
Andrii <andriychikulay@gmail.com>
2023-04-24 14:20:01 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const accordion = document.querySelectorAll('.container-ac');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+
+
