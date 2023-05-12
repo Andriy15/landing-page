647a1ae95ee6d702941896d3da665bf150a900a0
Andrii <andriychikulay@gmail.com>
2023-05-12 18:39:53 +0300

    fix some issues and add description

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const accordion = document.querySelectorAll('.container-ac');
+const accordion = document.querySelectorAll('.container-ac')
 
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
     this.classList.toggle('active')
   })
 }
+
