20dbf5a4cb7dc49f569623fde50a03018399b436
Andrii <andriychikulay@gmail.com>
2023-05-15 16:30:17 +0300

    description

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const accordion = document.querySelectorAll('.container-ac')
+const navSpan = document.querySelectorAll('.nav-span')
 
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
@@ -6,3 +7,16 @@ for (i=0; i<accordion.length; i++) {
   })
 }
 
+let prevSpan = null // keep track of previously clicked span
+
+navSpan.forEach((span) => {
+  span.addEventListener('click', () => {
+    if (prevSpan) {
+      prevSpan.style.borderBottom = ''
+      prevSpan.style.color = ''
+    }
+    span.style.borderBottom = '1px solid #1375ED'
+    span.style.color = '#1375ED'
+    prevSpan = span 
+  })
+})
